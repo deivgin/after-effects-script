05b07c2957c670284cd10a7e6b83d6029537449f
Deividas <deividasgineitis123@gmail.com>
2021-04-13 14:29:29 +0300

    Add Spinner

--- index.jsx
+++ index.jsx
@@ -36,17 +36,31 @@ var background = myComp.layers.addSolid(
   var spinner = myComp.layers.addShape();
   spinner.name = "spinner";
   spinner.property("Transform").property("Position").setValue([912, 544]);
-  //add ellipse
   var spinnerContent = spinner
     .property("Contents")
     .addProperty("ADBE Vector Group");
-  var spinnerGroup1 = spinner.property("Contents").property("Group 1");
+  spinnerContent.name = "Ellipse 1";
+  var spinnerGroup1 = spinner.property("Contents").property("Ellipse 1");
+  //Add Ellipse
   var spinnerEllipse1 = spinnerGroup1
     .property("Contents")
     .addProperty("ADBE Vector Shape - Ellipse");
+  spinnerEllipse1.property("Size").setValue([880, 880]); //Set Size
+  //Stroke
   var spinnerEllipseStroke = spinnerGroup1
     .property("Contents")
     .addProperty("ADBE Vector Graphic - Stroke");
   spinnerEllipseStroke.property("Color").setValue([0, 0, 0]); //STROKE COLOR
+  spinnerEllipseStroke.property("Stroke Width").setValue(30);
+  //Trim Paths
+  var spinnerEllipseTrimPaths = spinnerGroup1
+    .property("Contents")
+    .addProperty("ADBE Vector Filter - Trim");
+  spinnerEllipseTrimPaths.property("Start").expression =
+    "ease(time, 1, 2, 0, 10)";
+  spinnerEllipseTrimPaths.property("End").expression =
+    "ease(time, 0, 2, 0, 20)";
+  spinnerEllipseTrimPaths.property("Offset").expression =
+    "easeIn(time, 0, thisComp.duration, 0, 360 * thisComp.duration)";
   app.endUndoGroup();
 }
